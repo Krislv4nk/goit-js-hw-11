9033a1f3889cabf94b767f73707f0f88a8228954
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-14 12:42:10 +0100

    12-42

--- src/pixabay.js
+++ src/pixabay.js
@@ -1,27 +1,30 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
 
+export async function fetchImages(searchQuery, page = 1) {
+  const params = {
+    key: '40664862-84b34fcb53558e764c2f17e18',
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page,
+  };
 
-export function fetchImages(searchQuery, page = 1) {
-    const params = {
-      key: '40664862-84b34fcb53558e764c2f17e18',
-      q: searchQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: 40,
-      page,
-    };
-  
-    const url = 'https://pixabay.com/api/?' + new URLSearchParams(params).toString();
-    return axios.get(url)
-      .then(response => response.data.hits.map(image => ({
-        webformatURL: image.webformatURL,
-        largeImageURL: image.largeImageURL,
-        tags: image.tags,
-        likes: image.likes,
-        views: image.views,
-        comments: image.comments,
-        downloads: image.downloads,
-      })));
+  const url = 'https://pixabay.com/api/?' + new URLSearchParams(params).toString();
+  try {
+    const response = await axios.get(url);
+    return response.data.hits.map(image => ({
+      webformatURL: image.webformatURL,
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+      likes: image.likes,
+      views: image.views,
+      comments: image.comments,
+      downloads: image.downloads,
+    }));
+  } catch (error) {
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
   }
+}
